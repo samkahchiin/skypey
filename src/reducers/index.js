a0bfc1dff63f8381b1ecf94d68383d4288b3794d
samkahchiin <samkahchiin@gmail.com>
2020-06-15 01:06:18 +0800

    [refactor] Managed initial state in reducers instead of store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,6 @@
-export default (state, action) => {
+import { contacts } from '../static-data';
+
+// initial state of the apps is handler by reducers instead of store
+export default (state = { contacts }, action) => {
   return state;
 };
