502cb962a813dc0091831ea000bf75d98744c987
samkahchiin <samkahchiin@gmail.com>
2020-06-15 11:14:46 +0800

    [Finishes] Created Header for the chatwindow

--- src/containers/ChatWindow/ChatWindow.js
+++ src/containers/ChatWindow/ChatWindow.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Header from '../../components/Header/Header';
+import store from '../../store';
 
 const ChatWindow = ({ activeUserId }) => {
+  const state = store.getState();
+  const activeUser = state.contacts[activeUserId];
+
   return (
-    <div className='ChatWindow'>ChatWindow {activeUserId}</div>
+    <div className='ChatWindow'>
+      <Header user={activeUser}/>
+    </div>
   )
 }
 
