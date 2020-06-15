a9239c8f3e7a78def3305f170fb0601c5ddd8a4a
samkahchiin <samkahchiin@gmail.com>
2020-06-15 18:30:22 +0800

    Updates

---
+++ precache-manifest.cfcf1d12f3380a2b1d9ec0968da92b00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96fc8d15a93740d382600cf22e46a319",
+    "url": "/skypey/index.html"
+  },
+  {
+    "revision": "1ec50e6967bbed755703",
+    "url": "/skypey/static/css/main.23db451c.chunk.css"
+  },
+  {
+    "revision": "dd63e5c27b4d17e31785",
+    "url": "/skypey/static/js/2.c0824a03.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/skypey/static/js/2.c0824a03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ec50e6967bbed755703",
+    "url": "/skypey/static/js/main.96a2d0be.chunk.js"
+  },
+  {
+    "revision": "3197d6839fc6012762c7",
+    "url": "/skypey/static/js/runtime-main.bbe2af04.js"
+  }
+]);
